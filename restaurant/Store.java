4eee098646649adf408da42d97e379c69502aa37
Garrett <gahe0404@colorado.edu>
2020-10-10 19:14:21 -0600

    setting up user input from main

--- restaurant/Store.java
+++ restaurant/Store.java
@@ -10,7 +10,7 @@ public class Store {
 
     public Store(int numDays)
     {
-        System.out.println("In");
+        System.out.println("Welcome to Molly's Mouthwatering Rolls! The simulation is about to begin.");
 
         numSpring = 30;
         numEgg = 30;
@@ -19,6 +19,11 @@ public class Store {
         numJelly = 30;
 
         Menu ourMenu = new Menu();
+
+        for(int i=1 ; i<=numDays ; i++)
+        {
+            System.out.println("Today is Day " + i + ".");
+        }
 
     }
     
